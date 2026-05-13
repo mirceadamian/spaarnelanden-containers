9e2ff12b667e338e75b0818be4a792685e200fca
mirceadamian <dmircea@gmail.com>
2026-05-13 22:24:47 +0200

    Not needed to match into dtDateLastEmptied and perform datetime conversion to string, use SDataLastEmptied instead

--- custom_components/spaarnelanden/sensor.py
+++ custom_components/spaarnelanden/sensor.py
@@ -24,8 +24,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     }
 )
 
-CONTAINER_INFO_RE = r"var oContainerModel =(\[{.*?}\]);"
-DATE_RE = r"Date\((.*?)\)"
+CONTAINER_INFO_RE = r"var oContainerModel = (\[{.*?}\]);"
+# DATE_RE = r"Date\((.*?)\)"
 
 log = logging.getLogger(__name__)
 
@@ -91,13 +91,14 @@ class ContainerSensor(CoordinatorEntity):
     @callback
     def _handle_coordinator_update(self) -> None:
         container = self.coordinator.data[self.container_id]
-        last_emptied_match = re.search(DATE_RE, container["dtDateLastEmptied"])
-        last_emptied = int(last_emptied_match.group(1)) / 1000
+#        last_emptied_match = re.search(DATE_RE, container["dtDateLastEmptied"])
+#        last_emptied = int(last_emptied_match.group(1)) / 1000
         self.attrs = {
             const.ATTR_OUT_OF_USE: container["bIsOutOfUse"],
             const.ATTR_CAPACITY: container["dFillingDegree"],
             const.ATTR_ID: container["sRegistrationNumber"],
-            const.ATTR_LAST_EMPTIED: datetime.fromtimestamp(last_emptied),
+#            const.ATTR_LAST_EMPTIED: datetime.fromtimestamp(last_emptied),
+            const.ATTR_LAST_EMPTIED: container["sDateLastEmptied"],
             const.ATTR_CONTAINER_TYPE: container["sProductName"],
         }
         self._available = True
